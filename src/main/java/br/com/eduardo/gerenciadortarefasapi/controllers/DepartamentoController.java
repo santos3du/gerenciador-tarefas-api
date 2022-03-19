2709ef90954d713985d9da0803ff1cdeb19ef93d
Eduardo Santos <santosedu@live.com>
2022-03-19 15:31:08 -0300

    Ïnsert departamento

--- src/main/java/br/com/eduardo/gerenciadortarefasapi/controllers/DepartamentoController.java
+++ src/main/java/br/com/eduardo/gerenciadortarefasapi/controllers/DepartamentoController.java
@@ -1,13 +1,18 @@
 package br.com.eduardo.gerenciadortarefasapi.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.eduardo.gerenciadortarefasapi.dto.DepartamentoDTO;
 import br.com.eduardo.gerenciadortarefasapi.services.DepartamentoService;
@@ -31,6 +36,20 @@ public class DepartamentoController {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@PostMapping
+	public ResponseEntity<DepartamentoDTO> insert(DepartamentoDTO dto) {
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 	
--- src/main/java/br/com/eduardo/gerenciadortarefasapi/services/DepartamentoService.java
+++ src/main/java/br/com/eduardo/gerenciadortarefasapi/services/DepartamentoService.java
@@ -5,13 +5,17 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 import br.com.eduardo.gerenciadortarefasapi.dto.DepartamentoDTO;
 import br.com.eduardo.gerenciadortarefasapi.entities.Departamento;
 import br.com.eduardo.gerenciadortarefasapi.repositories.DepartamentoRepository;
+import br.com.eduardo.gerenciadortarefasapi.services.exceptions.DatabaseException;
 import br.com.eduardo.gerenciadortarefasapi.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,6 +35,25 @@ public class DepartamentoService {
 		Departamento departamento = optional.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado."));
 		return new DepartamentoDTO(departamento);
 		
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado. " + id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violarion");
+		}
+		
+	}
+
+	public DepartamentoDTO insert(DepartamentoDTO dto) {
+		Departamento entity = new Departamento();
+		entity.setTitulo(dto.getTitulo());
+		entity = repository.save(entity);
+		return new DepartamentoDTO(entity);
 	}
 
 }
